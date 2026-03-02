bfb33d6b1748177721988ff29459e96c238ca150
lilKriT <lilKriTwebDev@gmail.com>
2026-03-03 00:45:49 +0100

    Added files and styles.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Raleway, Open_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const ralewaySans = Raleway({
+  variable: "--font-raleway-sans",
   subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const openSans = Open_Sans({
+  variable: "--font-open-sans",
   subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${ralewaySans.variable} ${openSans.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,106 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <p>Features Team Sign In</p>
+
+      <p>All your files in one secure location, accessible anywhere.</p>
+
+      <p>
+        Fylo stores all your most important files in one secure location. Access
+        them wherever you need, share and collaborate with friends family, and
+        co-workers.
+      </p>
+
+      <p>Get Started</p>
+
+      <p>Access your files, anywhere</p>
+
+      <p>
+        The ability to use a smartphone, tablet, or computer to access your
+        account means your files follow you everywhere.
+      </p>
+
+      <p>Security you can trust</p>
+
+      <p>
+        2-factor authentication and user-controlled encryption are just a couple
+        of the security features we allow to help secure your files.
+      </p>
+
+      <p>Real-time collaboration</p>
+
+      <p>
+        Securely share files and folders with friends, family and colleagues for
+        live collaboration. No email attachments required.
+      </p>
+
+      <p>Store any type of file</p>
+
+      <p>
+        Whether you're sharing holidays photos or work documents, Fylo has you
+        covered allowing for all file types to be securely stored and shared.
+      </p>
+
+      <p>Stay productive, wherever you are</p>
+
+      <p>
+        Never let location be an issue when accessing your files. Fylo has you
+        covered for all of your file storage needs.
+      </p>
+
+      <p>
+        Securely share files and folders with friends, family and colleagues for
+        live collaboration. No email attachments required.
+      </p>
+
+      <p>See how Fylo works</p>
+
+      <p>
+        Fylo has improved our team productivity by an order of magnitude. Since
+        making the switch our team has become a well-oiled collaboration
+        machine.
+      </p>
+
+      <p>Satish Patel Founder & CEO, Huddle</p>
+
+      <p>
+        Fylo has improved our team productivity by an order of magnitude. Since
+        making the switch our team has become a well-oiled collaboration
+        machine.
+      </p>
+
+      <p>Bruce McKenzie Founder & CEO, Huddle</p>
+
+      <p>
+        Fylo has improved our team productivity by an order of magnitude. Since
+        making the switch our team has become a well-oiled collaboration
+        machine.
+      </p>
+
+      <p>Iva Boyd Founder & CEO, Huddle</p>
+
+      <p>Get early access today</p>
+
+      <p>
+        It only takes a minute to sign up and our free starter tier is extremely
+        generous. If you have any questions, our support team would be happy to
+        help you.
+      </p>
+
+      <p>Get Started For Free</p>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua
+      </p>
+
+      <p>+1-543-123-4567 example@fylo.com</p>
+
+      <p>About Us Jobs Press Blog</p>
+
+      <p>Contact Us Terms Privacy</p>
+    </div>
+  );
 }
