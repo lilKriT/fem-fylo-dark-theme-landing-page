85c8b3ed03f9a7d8ad8924674ae4cee97e76cad2
lilKriT <lilKriTwebDev@gmail.com>
2026-03-03 22:46:25 +0100

    Hero not bad.

--- app/_sections/Hero.tsx
+++ app/_sections/Hero.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Hero = () => {
   return (
     <section className="min-h-dvh flex justify-center items-center px-8 py-48 bg-navy-850 bg-[url(/bg-curvy-desktop.svg)] bg-no-repeat bg-bottom bg-contain">
-      <div className="flex flex-col justify-center items-center gap-8 max-w-3xl">
+      <div className="flex flex-col justify-center items-center gap-8 max-w-3xl text-center">
         <Image
           src={"/illustration-intro.png"}
           alt="Hero Image"
@@ -12,9 +12,11 @@ const Hero = () => {
           height={534}
           className="w-full"
         />
-        <h1>All your files in one secure location, accessible anywhere.</h1>
+        <h1 className="text-4xl font-bold leading-[1.25]">
+          All your files in one secure location, accessible anywhere.
+        </h1>
 
-        <p className="mt-4">
+        <p className="mt-4 max-w-[50ch]">
           Fylo stores all your most important files in one secure location.
           Access them wherever you need, share and collaborate with friends
           family, and co-workers.
